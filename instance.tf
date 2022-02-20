5aeda196f8b0f26262d8c4e2668cc7f669a69dbf
Preeti Kumari <47385113+kpreeti2588@users.noreply.github.com>
2022-02-20 13:08:23 +0530

    Update instance.tf

--- instance.tf
+++ instance.tf
@@ -20,7 +20,7 @@ resource "aws_instance" "hub88" {
   }
 
   provisioner "local-exec" {
-    command = "echo ${self.public_ip} username=Server${count.index+1} >> ansible/inventory"
+    command = "echo ${self.public_ip} username=Server${count.index+1} >> ansible/inventory"   #Creating ansible inventory from this command 
   }
 
   provisioner "local-exec" {
@@ -28,16 +28,17 @@ resource "aws_instance" "hub88" {
   }
 }
 
-resource "time_sleep" "wait_2_min" {
-  depends_on = [aws_lb.lb]
+#resource "time_sleep" "wait_2_min" {  ##Commented out since its making terraform to wait for 2 minutes since EC2 provsioning is taking (1-2 mins) time to execute 
+#  depends_on = [aws_lb.lb]   # It is useful when we want ansible command also runs along with terraform script. It is giving time to instances for warm up
 
-  create_duration = "60s"
-}
+#  create_duration = "120s"
+#}
 
 resource "null_resource" "test-ans"{
-    depends_on= [time_sleep.wait_2_min]
+	depends_on = [aws_lb.lb]   ## this is the last thing gets created in terraform graph. So, Ansible command is dependent on this 
+#	depends_on= [time_sleep.wait_2_min]  
 	
 	provisioner "local-exec" {
-		command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook ansible/install_docker.yaml ansible/install_nginx.yaml -i ansible/inventory --private-key '${file(var.PATH_TO_PRIVATE_KEY)}' -e log_group=${var.log_group_name} -e ansible_user=ubuntu"
+		command = "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook ansible/install_docker.yaml ansible/install_nginx.yaml -i ansible/inventory --private-key '${file(var.PATH_TO_PRIVATE_KEY)}' -e 'log_group=${var.log_group_name} ansible_user=ubuntu' "
 	}
 }
